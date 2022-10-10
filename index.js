f67a9be83cf4c053a397dd2be12237d9ba15a859
Sneha-Sasmal-2001 <100212890+Sneha-Sasmal-2001@users.noreply.github.com>
2022-10-10 15:56:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+//let show = document.getElementById("error");
+
+ function show_error(){
+    //show.innerText = "Something went wrong! Please try again later.";
+    document.getElementById("error").innerText = "Something went wrong! Please try again later.";
+ }
